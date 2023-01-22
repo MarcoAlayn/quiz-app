0445bb8303f8a047b4fb303160eff9d777c302c4
MarcoAlayn <marc_rob92@hotmail.com>
2023-01-22 01:18:20 -0700

    handlerchange

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, ReactEventHandler, useState } from "react";
 import preguntas from "../src/questions/questions.json";
 
 interface Pregunta {
@@ -15,14 +15,36 @@ interface Pregunta {
 function App() {
   const [preguntasList, setPreguntasList] = useState<Pregunta[]>(preguntas);
   const [preguntaActual, setPreguntaActual] = useState<number>(0);
-  const currentCuestion = Array(preguntasList[preguntaActual]);
+  const [isCorrect, setIsCorrect] = useState<boolean>(false);
+  const [preguntaElegida, setPreguntaElegida] = useState<string>("");
 
-  console.log(currentCuestion);
+  const currentCuestion = Array(preguntasList[preguntaActual]);
+  const handlerChange = (e: ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
+    return setPreguntaElegida(e.target.value);
+  };
 
   return (
     <div>
       {currentCuestion.map((item) => (
-        <span key={item.pregunta}>{item.tema}</span>
+        <>
+          <span key={item.pregunta}>{item.tema}</span>
+          <span>{item.pregunta}</span>
+          <span>
+            {item.respuestas.map((option) => (
+              <div key={option}>
+                <input
+                  type="radio"
+                  name={item.pregunta}
+                  value={option}
+                  checked={preguntaElegida === option}
+                  onChange={handlerChange}
+                />
+                <span>{option}</span>
+              </div>
+            ))}
+          </span>
+        </>
       ))}
       <button
         type="button"
